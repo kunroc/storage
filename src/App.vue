85e25c998b4d372eda22c722b5f4db6940404113
Your Name <your-email@example.com>
2025-07-17 15:29:13 +0800

    chore: init vue3 vite hello world project (ignore dist & node_modules)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="center">hello world</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="less">
+.center {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2.5rem;
+  font-weight: bold;
+}
+</style> 
